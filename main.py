2dc36cbe2991d6a3242360d76cbe1a1f2d7806d9
CodingCurtis42 <codingcurtis42@gmail.com>
2021-11-13 19:51:23 -0800

    Initial setup and commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import logging 
+
+app = Flask(__name__)
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename="./Logfiles/log.log", level=logging.DEBUG, format = LOG_FORMAT)
+logger = logging.getLogger()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
