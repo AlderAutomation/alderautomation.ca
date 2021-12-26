928919448cfdbca1aea6cc0de83d519a75676cf7
CodingCurtis42 <codingcurtis42@gmail.com>
2021-12-26 10:38:45 -0800

    changing website reposiroty from CC42 to AA

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(filename="./Logfiles/log.log", level=logging.DEBUG, format = LOG_FORMAT)
 logger = logging.getLogger()
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('/projects.html')
 
 
 if __name__ == "__main__":
