9141c697eb67710bfee256249afd43053dcfd6fc
CodingCurtis42 <codingcurtis42@gmail.com>
2021-12-31 16:43:03 -0800

    Restructure into one page

--- main.py
+++ main.py
@@ -12,14 +12,5 @@ logger = logging.getLogger()
 def home():
     return render_template('home.html')
 
-@app.route('/projects')
-def projects():
-    return render_template('/projects.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('/contact.html')
-
-
 if __name__ == "__main__":
     app.run(debug=True)
