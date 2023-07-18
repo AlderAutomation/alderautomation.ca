f43db065a5475bf1e130fd49f53c6be19bb3d9fa
AlderAutomation <mheltman@alderautomation.ca>
2023-07-18 12:48:46 -0700

    Style. clean main. add routes and pages.

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from crypt import methods
-from flask import Flask, redirect, render_template, request, send_file
+from flask import Flask, render_template, request
 import logging 
 import smtplib, config
-from email.message import EmailMessage
-import mimetypes
-from email.mime.multipart import MIMEMultipart
-from email import encoders
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-
 
 app = Flask(__name__)
 
@@ -19,46 +11,22 @@ logger = logging.getLogger()
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        subject = request.form['subject']
-        message = request.form['message']
-
-        send_email(name, email, subject, message)
-        
     return render_template('index.html')
 
 
-def send_email(name, email, subject, message):
-    sendfrom = 'alderautomation@shaw.ca'
-    sendto = 'info@alderautomation.ca'
-    user = config.user
-    pwd = config.password
-
-    try:
-        msg = MIMEMultipart()
-        msg['From'] = sendfrom
-        msg['To'] = sendto
-        msg['Subject'] = subject
-        msg.preamble = subject
-        body = 'Here is a new message from ' + name + ' at ' + email + '\n\n' + message
-        body = MIMEText(body) 
-        msg.attach(body) 
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    return render_template('about.html')
 
 
-        server = smtplib.SMTP('mail.shaw.ca', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(user, pwd)
-        server.send_message(msg)
-    except Exception as e:
-        logger.exception(e)
-        print('Error')
-    finally:
-        if server is not None:
-            server.quit()
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    return render_template('contact.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
